dc9fefcfcab56db6cb9facd688e912ec33d93826
Parvina <mirakhmedova89@gmail.com>
2025-03-20 11:53:06 +0500

    add changes

--- queries/patients.sql
+++ queries/patients.sql
@@ -7,12 +7,3 @@ select
     , occupation 
     , address
 from patients;
-
-select * from patients_1
-where address is null;
-
-select * from diseases_1
-where bmi is null;
-
-select* from diabetic_retinopathy_1
-where fbg is null;
